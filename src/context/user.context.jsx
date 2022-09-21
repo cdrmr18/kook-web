dfa4f198bbc63d0f6b299ccc8b7936c3dcd25649
Kathy Tavia <kathy.tavia@gmail.com>
2022-09-21 17:19:02 +0200

    add user reducer

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
@@ -7,12 +7,35 @@ import {
 // actual value to access
 export const UserContext = createContext({
   currentUser: null,
-  setCUrrentUser: () => {},
+  setCurrentUser: () => {},
 });
 
+// user action type
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
+// reducer for user
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+    default:
+      return state;
+  }
+};
+// reducer initial value
+const INITIAL_VALUE = {
+  currentUser: null,
+};
 //  actual component
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [state, dispatch] = useReducer(userReducer, INITIAL_VALUE);
+
+  const { currentUser } = state;
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: SET_CURRENT_USER, payload: user });
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {

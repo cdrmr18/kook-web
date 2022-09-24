342b30d7334289512250aba527c88ebf8f7a8d3e
Kathy Tavia <kathy.tavia@gmail.com>
2022-09-24 21:40:28 +0200

    implemented redux-persist

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { PersistGate } from "redux-persist/integration/react";
+
 import "./index.css";
 import App from "./App";
 
@@ -11,11 +13,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <ChakraProvider>
-          <App />
-        </ChakraProvider>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <ChakraProvider>
+            <App />
+          </ChakraProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+
 import { userReducer } from "./user/userReducer";
 import { chefsReducer } from "./chefs/chefsReducer";
 import { recipesReducer } from "./recipes/recipesReducer";
